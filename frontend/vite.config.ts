6e6c1dd7491eb8f13d68161661158ed550764556
PierreNouhen <pierre.nouhen@orange.fr>
2025-08-01 16:23:32 +0200

    feat: deployement vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser'
+  },
+  base: './' // Important pour les chemins relatifs sur Vercel
 })
