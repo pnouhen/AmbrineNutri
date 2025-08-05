01e1d5272c8519bbb7a48fd4e45fcfe83e2aa6dd
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-08-05 15:33:55 +0200

    fix: css filter

--- frontend/src/recipes/RecipeFilter.jsx
+++ frontend/src/recipes/RecipeFilter.jsx
@@ -8,14 +8,7 @@ export function RecipeFilter({ data, filter, setFilter }) {
       <h2 className="h2 mb-5">Catégories</h2>
       <ul className="m-auto lg:grid lg:grid-cols-3 flex flex-wrap justify-center lg:gap-2.5 gap-8">
         {data.map((categorie, index) => (
-          <li
-            key={index}
-            className={`lg:mx-auto ${
-              index === data.length - 1
-                ? "col-start-1 col-end-4"
-                : ""
-            }`}
-          >
+          <li key={index} className="lg:mx-auto">
             <button
               className={`text recipeButton ${
                 filter === categorie ? "recipeButtonActive" : ""
